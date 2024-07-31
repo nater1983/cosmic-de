7f7551fb098d2d31b7a9a3c25fe5ad32d2d1463b
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2024-07-30 21:20:17 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ cosmic-applet-host
+ cosmic-applets
+ cosmic-applibrary
+ cosmic-bg
+ cosmic-comp
+ cosmic-launcher
+ cosmic-osd
+ cosmic-panel
+ cosmic-workspaces-epoch
+ cosmic-session
+ cosmic-settings-daemon
+ xdg-desktop-portal-cosmic
